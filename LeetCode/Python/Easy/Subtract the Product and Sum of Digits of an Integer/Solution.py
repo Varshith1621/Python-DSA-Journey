781e1b9aeafffa7af19a3b3ce41762be12045013
Varshith <180854390+Varshith1621@users.noreply.github.com>
2026-07-14 09:26:01 +0530

    Add Easy Python Solution from LeetCode

--- LeetCode/Python/Easy/Subtract the Product and Sum of Digits of an Integer/Solution.py
+++ LeetCode/Python/Easy/Subtract the Product and Sum of Digits of an Integer/Solution.py
@@ -1,1 +1,12 @@
-n
+class Solution(object):
+    def subtractProductAndSum(self, n):
+        sum_digit = 0
+        product_digit = 1
+
+        while n > 0:
+            digit = n%10
+            sum_digit += digit
+            product_digit *=digit
+
+            n//=10
+        return product_digit - sum_digit
