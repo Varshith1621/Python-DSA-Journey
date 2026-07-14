29e320d618232a7ba7bcb83138d82099690a5b88
Varshith <180854390+Varshith1621@users.noreply.github.com>
2026-07-14 10:06:12 +0530

    Add Medium Python Solution from LeetCode

--- LeetCode/Python/Medium/Single Element in a Sorted Array/Solution.py
+++ LeetCode/Python/Medium/Single Element in a Sorted Array/Solution.py
@@ -1,7 +1,12 @@
 class Solution(object):
     def singleNonDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if mid % 2 == 1:
+                mid -= 1
+            if nums[mid] != nums[mid + 1]:
+                right = mid
+            else:
+                left = mid + 2
+        return nums[left]
